ddfaa4c4d8d36f9329e0d0e4053fa5ee7851b737
hgfejcwoefGleb <114211284+hgfejcwoefGleb@users.noreply.github.com>
2025-09-15 21:30:55 +0300

    Update settings.py

--- quotes/quotes/settings.py
+++ quotes/quotes/settings.py
@@ -36,8 +36,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "catalog.apps.CatalogConfig",
-    "catalog",
+    "quotes.catalog.apps.CatalogConfig",
+
 ]
 
 MIDDLEWARE = [
